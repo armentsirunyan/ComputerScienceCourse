5eb8fd035aa1afcd5f5e47a656ba6da6ff3725ac
Armen Tsirunyan <armen.tsirunyan@gmail.com>
2014-05-03 22:58:53 +0400

    Basic Carcass

---
+++ CreateDB.php
@@ -0,0 +1,12 @@
+<?php
+require_once('base.php');
+/*
+mysql_query($query, $conn) or die ("Unable to create the database ".$dbname.": ".mysql_error($conn));
+echo ('<p class = "phpdebug">Successfully created database '.$dbname."</p>");
+//use database
+mysql_select_db($dbname, $conn) or die ("Unable to select database ".$dbname);
+*/
+
+//create table Users
+
+?>
---
+++ base.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+
+$dbhost = "localhost"; 
+$dbname = "RauComputerScience"; 
+$dbuser = "root"; 
+$dbpass = "";
+
+
+?>
