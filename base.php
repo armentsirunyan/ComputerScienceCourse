98fc5171fbe9b942158660425ebe6570e7242355
Armen Tsirunyan <armen.tsirunyan@gmail.com>
2014-05-04 00:16:53 +0400

    A test commit

--- base.php
+++ base.php
@@ -2,9 +2,14 @@
 session_start();
 
 $dbhost = "localhost"; 
-$dbname = "RauComputerScience"; 
+$dbname = "ComputerScienceCourse"; 
 $dbuser = "root"; 
 $dbpass = "";
 
+$sqllink = mysql_connect($dbhost, $dbuser, $dbpass)
+	or die("Unable to connect to MySQL");
+	
+mysql_select_db($dbname, $sqllink)
+	or die("Ubable to select database ".$dbname);
 
 ?>
